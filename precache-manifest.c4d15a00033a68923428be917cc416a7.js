d19fba13ab29aa6751fc1beab66ab1ec296c4f8d
RodriBlanca <rodrigoblanca96@gmail.com>
2022-06-24 21:29:17 -0300

    Updates

---
+++ precache-manifest.c4d15a00033a68923428be917cc416a7.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "4decea7b8e95c1b6c7c9",
+    "url": "/brusi-propiedades/static/css/main.d81ac3b8.chunk.css"
+  },
+  {
+    "revision": "4decea7b8e95c1b6c7c9",
+    "url": "/brusi-propiedades/static/js/main.4decea7b.chunk.js"
+  },
+  {
+    "revision": "960b210e2f30d87d89af",
+    "url": "/brusi-propiedades/static/js/1.960b210e.chunk.js"
+  },
+  {
+    "revision": "72581ee2a3bc6dd599a0",
+    "url": "/brusi-propiedades/static/js/runtime~main.72581ee2.js"
+  },
+  {
+    "revision": "b78f03f40d3786522d14ad0d0fdf053e",
+    "url": "/brusi-propiedades/static/media/logo-brusi-propiedades-blanco.b78f03f4.png"
+  },
+  {
+    "revision": "46ad79f517c174eccc2b14d39b666926",
+    "url": "/brusi-propiedades/static/media/video-background.46ad79f5.mp4"
+  },
+  {
+    "revision": "59d8f3300f9091240ac251e276abda8f",
+    "url": "/brusi-propiedades/static/media/Barrio Brickland-53.59d8f330.jpg"
+  },
+  {
+    "revision": "182dfca2184477c9e4aa45e13b205951",
+    "url": "/brusi-propiedades/static/media/undraw_secure_files_re_6vdh.182dfca2.svg"
+  },
+  {
+    "revision": "c948ab4b2d50f51650fdc6acce1d70ff",
+    "url": "/brusi-propiedades/static/media/undraw_emails_re_cqen.c948ab4b.svg"
+  },
+  {
+    "revision": "07ff1742ef2c3244c474095d2028141f",
+    "url": "/brusi-propiedades/static/media/undraw_house_searching_re_stk8.07ff1742.svg"
+  },
+  {
+    "revision": "6f7cf344ad2a3159dfeb87c659ead3e2",
+    "url": "/brusi-propiedades/static/media/undraw_business_plan_re_0v81.6f7cf344.svg"
+  },
+  {
+    "revision": "692a5c2637b90d75d6ebfc7d212a418d",
+    "url": "/brusi-propiedades/static/media/undraw_social_dashboard_re_ocbd.692a5c26.svg"
+  },
+  {
+    "revision": "364829cd6f40262c431351af9c7388c3",
+    "url": "/brusi-propiedades/static/media/undraw_moments_0y20.364829cd.svg"
+  },
+  {
+    "revision": "b51199fc63f3296ba300d7b900fe3816",
+    "url": "/brusi-propiedades/static/media/undraw_security_on_re_e491.b51199fc.svg"
+  },
+  {
+    "revision": "0d438fb9bd19204815a4e4c96f34630d",
+    "url": "/brusi-propiedades/static/media/undraw_business_deal_re_up4u.0d438fb9.svg"
+  },
+  {
+    "revision": "3c1ba4df32fcdf126c8e27716ccda21a",
+    "url": "/brusi-propiedades/static/media/undraw_team_spirit_re_yl1v.3c1ba4df.svg"
+  },
+  {
+    "revision": "31dbea523b26e51b5214cecec6f4a700",
+    "url": "/brusi-propiedades/static/media/undraw_abstract_re_l9xy.31dbea52.svg"
+  },
+  {
+    "revision": "3ddf6c78a810035703102cd61fc23081",
+    "url": "/brusi-propiedades/static/media/undraw_website_builder_re_ii6e.3ddf6c78.svg"
+  },
+  {
+    "revision": "7924293a9f0ec9c5d5357a50dbf903eb",
+    "url": "/brusi-propiedades/static/media/undraw_mobile_re_q4nk.7924293a.svg"
+  },
+  {
+    "revision": "2e3c480f453e67bbaffc92b0fa457d9a",
+    "url": "/brusi-propiedades/static/media/undraw_certificate_re_yadi.2e3c480f.svg"
+  },
+  {
+    "revision": "ac44cbcd75551f92d5496418af309935",
+    "url": "/brusi-propiedades/static/media/undraw_follow_me_drone_kn76.ac44cbcd.svg"
+  },
+  {
+    "revision": "bf80f93820a39c98acee66cbe11cf982",
+    "url": "/brusi-propiedades/static/media/undraw_designer_girl_re_h54c.bf80f938.svg"
+  },
+  {
+    "revision": "6ed48170e17264633afb8822e65545a2",
+    "url": "/brusi-propiedades/static/media/undraw_video_files_fu10.6ed48170.svg"
+  },
+  {
+    "revision": "e2916b3505a2059db067da361c8ec5fc",
+    "url": "/brusi-propiedades/static/media/undraw_shared_goals_re_jvqd (1).e2916b35.svg"
+  },
+  {
+    "revision": "167e367417b28c2bd5652a58b0032622",
+    "url": "/brusi-propiedades/static/media/undraw_select_house_qbag.167e3674.svg"
+  },
+  {
+    "revision": "742d9050061d43aeec7d7565e06c6432",
+    "url": "/brusi-propiedades/static/media/undraw_stepping_up_g6oo.742d9050.svg"
+  },
+  {
+    "revision": "c28071fcea9742f039d0ae44468caf22",
+    "url": "/brusi-propiedades/static/media/tiktok-icon.c28071fc.png"
+  },
+  {
+    "revision": "19455d676899e370bd5cc0bab6b41c6f",
+    "url": "/brusi-propiedades/static/media/whatsapp-icon.19455d67.png"
+  },
+  {
+    "revision": "4f7cb5fbb28d0f9ddc97641f8b288735",
+    "url": "/brusi-propiedades/static/media/instagram-icon.4f7cb5fb.png"
+  },
+  {
+    "revision": "4aa43a7d58e9899c0d0bcbf516285e02",
+    "url": "/brusi-propiedades/static/media/youtube-icon.4aa43a7d.png"
+  },
+  {
+    "revision": "e03e93e35cc3d983ed6d5c877d2244f2",
+    "url": "/brusi-propiedades/static/media/jonathan-brusi.e03e93e3.jpeg"
+  },
+  {
+    "revision": "1d0549ea530c3d3cb407d1f6fbdd3ed6",
+    "url": "/brusi-propiedades/static/media/gustavo-seneca.1d0549ea.jpeg"
+  },
+  {
+    "revision": "2b20831518722f46e2f0d701c8ad0ea2",
+    "url": "/brusi-propiedades/static/media/undraw_notify_re_65on.2b208315.svg"
+  },
+  {
+    "revision": "123bec96369b636679d6493cca0cf205",
+    "url": "/brusi-propiedades/index.html"
+  }
+];
